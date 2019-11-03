ef125447ece337c82a90f6432aa6209e73e2077d
aclouie89 <43075684+aclouie89@users.noreply.github.com>
2019-11-02 19:00:51 -0500

    skeleton for open.py

---
+++ code/open/open.py
@@ -0,0 +1,23 @@
+def open(image, threshold, window):
+  return image
+
+def close(image, threshold, window):
+  return image
+
+def erosion(image, threshold, window):
+  return image
+
+def dilation(image, threshold, window):
+  return image
+
+def setWindow(matrix, row_index, col_index):
+  matrix[row_index][col_index] = 1
+  return matrix
+
+def setPlusWindow()
+  window = setWindow(window, 1,0 )
+  window = setWindow(window, 1,1 )
+  window = setWindow(window, 1,2 )
+  window = setWindow(window, 0,1 )
+  window = setWindow(window, 2,1 )
+  return window
